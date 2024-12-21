f753ac8ce84e8e9f8ccba30308f2869aaf6f13b2
HyoungukJo <whguddnr9@gmail.com>
2024-12-21 17:14:18 +0900

    refactor: stateful한 싱글톤 생성

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -9,7 +9,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatefulServiceTest {
-
     @Test
     void statefulServiceSingleton() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
